6e161c7b4599d66126253678a05ee211f5cce35b
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2024-02-22 13:30:29 +0100

    Change CO2-price. introduce fields for it.

--- src/main/java/org/tub/vsp/bvwp/computation/Modifications.java
+++ src/main/java/org/tub/vsp/bvwp/computation/Modifications.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 
 public record Modifications(double co2Price, double mehrFzkm) {
     private static final Logger log = LogManager.getLogger(Modifications.class);
+    private static final double co2PriceBVWP = 145.;
+    private static final double co2PriceIncreased = 623.;
 
     public Modifications {
         if (co2Price < 145) {
@@ -13,15 +15,17 @@ public record Modifications(double co2Price, double mehrFzkm) {
     }
 
     public static Modifications createInducedWithAdditionalFzkm( double mehrFzkm ) {
-        return new Modifications( 145., mehrFzkm);
+        return new Modifications( co2PriceBVWP, mehrFzkm);
     }
 
     public static Modifications createInducedAndCo2WithMehrFzkm(double mehrFzkm) {
-        return new Modifications( 5 * 145., mehrFzkm);
+        return new Modifications( co2PriceIncreased, mehrFzkm);
     }
 
-    public static final Modifications NO_CHANGE = new Modifications( 145., 0.);
-    public static final Modifications CO2_PRICE = new Modifications( 5. * 145., 0.);
+
+    public static final Modifications NO_CHANGE = new Modifications(co2PriceBVWP, 0.);
+
+    public static final Modifications CO2_PRICE = new Modifications(co2PriceIncreased, 0.);
 
     @Override public String toString() {
         return "[co2Price=" + co2Price + "; mehrFzkm=" + mehrFzkm + "]";
