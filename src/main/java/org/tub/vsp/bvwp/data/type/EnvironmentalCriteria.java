1cb5fd7d01e2b470313863fbcf004bd0b7217637
Paul Heinrich <pheinrich11235@gmail.com>
2024-07-12 15:22:53 +0200

    scrape environmental stuff

---
+++ src/main/java/org/tub/vsp/bvwp/data/type/EnvironmentalCriteria.java
@@ -0,0 +1,12 @@
+package org.tub.vsp.bvwp.data.type;
+
+import java.util.List;
+
+public record EnvironmentalCriteria(List<Description> description, UmweltBewertung bewertung) {
+    public record Description(double absolute, double betroffenheit) {
+    }
+
+    public enum UmweltBewertung {
+        HOCH, MITTEL, GERING;
+    }
+}
